dd518cfcad63182cb0764bac4caa2d11c3ec5992
Sean Hsieh <hsiehs@fortinet.com>
2023-11-15 15:10:53 +0800

    Stand-alone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
